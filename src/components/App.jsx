d949fffd286e6b0798aed5ef502d1a68d11a6b7c
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-09-05 14:17:08 -0500

    completed app Utilized: - callback functions - spread operators - data mapping - useState - other shit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [newListItem, setNewListItem] = useState();
+
+  const [allToDoItems, setAllToDoItems] = useState([
+    "start2",
+    "continue2",
+    "finish2"
+  ]);
+
+  const toDoItems = ["start", "continue", "finish"];
+
+  function handleListItem(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setNewListItem(value);
+    console.log(newListItem);
+  }
+
+  function addItem() {
+    setAllToDoItems((prevValue) => {
+      return [...prevValue, newListItem];
+    });
+    setNewListItem("");
+    console.log(allToDoItems);
+  }
+
+  function createList(toDoListItem) {
+    return <li>{toDoListItem}</li>;
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          name="newToDoItemContent"
+          type="text"
+          onChange={handleListItem}
+          value={newListItem}
+        />
+        <button type="submit" name="addNewToDoItem" onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
-        <ul>
-          <li>A Item </li>
-        </ul>
+        <ul>{allToDoItems.map(createList)}</ul>
       </div>
     </div>
   );
